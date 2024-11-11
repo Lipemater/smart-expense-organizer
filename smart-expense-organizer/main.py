0d489e4b065f6be26150c81156e757f30a2d88dc
Lipemater <149597488+Lipemater@users.noreply.github.com>
2024-11-11 09:14:12 -0300

    Delete smart-expense-organizer directory

--- smart-expense-organizer/main.py
+++
@@ -1,6 +0,0 @@
-from Interface import criar_interface
-from processamento import processar_arquivos
-
-# Iniciar o programa chamando a interface e passando a função de processamento
-if __name__ == "__main__":
-    criar_interface()
